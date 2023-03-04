aecc4cfdcad92f1f1b62c59da1c0cfaa333b4d69
Aranya <72189043+aranya2001@users.noreply.github.com>
2023-03-04 23:03:26 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Store - Home Page</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Welcome to our Online Store!</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Products</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<section id="banner">
+			<h2>Shop for Grocery Items, Eggs, and Frozen Meats</h2>
+			<p>Get everything you need for your next meal right here in our online store.</p>
+			<button>Shop Now</button>
+		</section>
+
+		<section id="featured-products">
+			<h3>Featured Products</h3>
+			<ul>
+				<li>
+					<img src="grocery-item.jpg" alt="Grocery Item">
+					<h4>Grocery Item</h4>
+					<p>Price: $10.00</p>
+					<button>Add to Cart</button>
+				</li>
+				<li>
+					<img src="eggs.jpg" alt="Eggs">
+					<h4>Eggs</h4>
+					<p>Price: $5.00</p>
+					<button>Add to Cart</button>
+				</li>
+				<li>
+					<img src="frozen-meat.jpg" alt="Frozen Meat">
+					<h4>Frozen Meat</h4>
+					<p>Price: $15.00</p>
+					<button>Add to Cart</button>
+				</li>
+			</ul>
+		</section>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 Online Store. All rights reserved.</p>
+	</footer>
+</body>
+</html>
